b8197379785c9701470399cf2920acac514a7b1c
dragon-zhang <zhangzicheng@apache.org>
2025-03-23 16:04:59 +0800

    simplify code

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -41,7 +41,7 @@ if [ "${NO_RUN}" != "1" ] && [ "${NO_RUN}" != "true" ]; then
         # todo test it
         "${CARGO}" build --target "${TARGET}" --no-default-features --features "async-cancel,bytes,iocp,macros,utils"
         "${CARGO}" build --target "${TARGET}" --no-default-features --features "async-cancel,bytes,iocp,macros,utils" --release
-    end
+    fi
 
     if [ "${TARGET}" != "aarch64-unknown-linux-gnu" ] && [ "${TARGET}" != "armv7-unknown-linux-gnueabihf" ] &&
         [ "${TARGET}" != "riscv64gc-unknown-linux-gnu" ] && [ "${TARGET}" != "s390x-unknown-linux-gnu" ] &&
